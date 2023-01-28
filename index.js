293753a75784522eaeb363e3c0da1cef295bbece
Vedad <vedadz@gmail.com>
2023-01-28 21:25:41 +0100

    Added marked and changed server port
    * changed server port to listen on 5080
    * added "marked" for formatting
    * added open.pug template for opening hours

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { loadMovie, loadMovies } from "./js/loadMoviesFromApi.js";
+import { marked } from "marked";
 
 const app = express();
+app.locals.marked = marked;
+
 app.set("view engine", "pug");
 app.set("views", "./templates");
 
@@ -18,16 +21,24 @@ app.get("/movies",async(req,res) => {
 app.get("/movies/:movieId" ,async (req,res) => {
 	try {
 		const movie = await loadMovie(req.params.movieId);
-		res.render('movie', {movie: movie});
+		res.render("movie", {movie: movie});
 	} catch {
 		res.status(404).render("404");
 	}
 });
 
+app.get("/open", (req, res) => {
+	res.render("open");
+});
+
 app.get("/placeholder", (req,res) => {
-	res.render('placeholder');
-})
+	res.render("placeholder");
+});
+
+app.get("/:somePath", (req,res) => {
+	res.status(404).render("404");
+});
 
 app.use('/static', express.static('./static'));
 
-app.listen(3080);
+app.listen(5080);
