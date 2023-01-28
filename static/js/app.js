b006afd9a101b5e914687563e53ba855dab023ee
Vedad <vedadz@gmail.com>
2023-01-28 23:31:37 +0100

    added test
    * included jest and supertest in project
    * made some basic test for the site
    * moved functionality of server into app.js , so it can be tested while te server is running.

---
+++ static/js/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { loadMovie, loadMovies } from "./loadMoviesFromApi.js";
+import { marked } from "marked";
+
+const app = express();
+app.locals.marked = marked;
+
+app.set("view engine", "pug");
+app.set("views", "./templates");
+
+app.get("/", async (req,res) => {
+	const movies = await loadMovies();
+	res.render('index', { movies: movies });
+});
+
+app.get("/movies",async(req,res) => {
+	const movies = await loadMovies();
+	res.render('movies', { movies: movies });
+});
+
+app.get("/movies/:movieId" ,async (req,res) => {
+	try {
+		const movie = await loadMovie(req.params.movieId);
+		res.render("movie", {movie: movie});
+	} catch {
+		res.status(404).render("404");
+	}
+});
+
+app.get("/open", (req, res) => {
+	res.render("open");
+});
+
+app.get("/placeholder", (req,res) => {
+	res.render("placeholder");
+});
+
+app.get("/:somePath", (req,res) => {
+	res.status(404).render("404");
+});
+
+app.use('/static', express.static('./static'));
+
+export default app;
