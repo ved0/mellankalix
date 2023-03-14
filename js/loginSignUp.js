0f09ec91fb57c213b9f3ccc57050e3082820e5b6
Vedad <vedadz@gmail.com>
2023-03-14 14:38:50 +0100

    Login and Signup design
    *Made Login and Sign-up with help of Google Materials.
    *For now only front-end and with some alerts displaying when Logging in and/or registering
    * after successful login in, it changeds the content of the button and it becomes unclickable
    * registration has some checks for name, email and password (it has to contain one small letter, one large letter and one number. Needs also to be at least 8 characters)

---
+++ js/loginSignUp.js
@@ -0,0 +1,71 @@
+const logInMenuButton = document.querySelector(".login-signup");
+const mainContainer = document.querySelector(".login-sign-up-container");
+const signUpContainer = document.querySelector(".sign-up-container");
+const loginContainer = document.querySelector(".login");
+const username = document.querySelector("#username");
+const registerNewUser = document.querySelector(".register-me");
+const logInForm = document.querySelector(".login-form");
+const signUpForm = document.querySelector(".sign-up-form");
+const newPassword = document.querySelector("#new-password");
+const confirmPassword = document.querySelector("#password-confirm");
+
+const closeLoginWindowButton = document.querySelector(".close-login");
+const closeRegistrationWindowButton = document.querySelector(".close-register");
+
+logInMenuButton.addEventListener("click", () => {
+  signUpContainer.classList.remove("show");
+  loginContainer.classList.remove("show");
+  mainContainer.classList.toggle("show");
+  loginContainer.classList.toggle("show");
+});
+
+logInForm.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    alert("Nu är du inloggad!");
+    closeLoginWindow();
+    logInMenuButton.textContent = "Inloggad som: " + username.value;
+    logInMenuButton.disabled = true;
+    logInMenuButton.style.cursor = "not-allowed";
+    logInMenuButton.style.backgroundColor = "#9e2a2b";
+});
+
+closeLoginWindowButton.addEventListener("click",closeLoginWindow);
+
+registerNewUser.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  loginContainer.classList.remove("show");
+  signUpContainer.classList.toggle("show");
+});
+
+closeRegistrationWindowButton.addEventListener("click", closeRegistrationWindow);
+
+signUpForm.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    alert("Nu är du registrerad! Välkommen till klubben!");
+    closeRegistrationWindow();
+});
+
+function comparePassword(){
+    if (newPassword.value != confirmPassword.value) {
+        confirmPassword.setCustomValidity("Lösenorden matchar inte!");
+      } else {
+        confirmPassword.setCustomValidity("");
+      }
+}
+
+newPassword.onchange = comparePassword;
+confirmPassword.onkeyup = comparePassword;
+
+function closeRegistrationWindow(){
+    mainContainer.classList.remove("show");
+    signUpContainer.classList.remove("show");
+    mainContainer.classList.toggle("hide");
+    signUpContainer.classList.toggle("hide");
+}
+
+function closeLoginWindow() {
+    mainContainer.classList.remove("show");
+    loginContainer.classList.remove("show");
+    mainContainer.classList.toggle("hide");
+    loginContainer.classList.toggle("hide");
+}
